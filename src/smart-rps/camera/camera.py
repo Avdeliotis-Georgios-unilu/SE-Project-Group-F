1c5e136a592e329144f87026934592fb24e3eab0
Georgios Avdeliotis <georgios.avdeliotis.001@srudent.uni.lu>
2026-03-27 23:59:15 +0100

    camera

--- src/smart-rps/camera/camera.py
+++ src/smart-rps/camera/camera.py
@@ -2,7 +2,7 @@ import cv2
 
 class Camera:
     def __init__(self, device_index=0, width=640, height=480):
-        self.cap = cv2.VideoCapture(device_index) # 0 = first USB cam
+        self.cap = cv2.VideoCapture(device_index)  # 0 = first USB cam
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         self.cap.set(cv2.CAP_PROP_FPS, 60)
@@ -15,9 +15,10 @@ class Camera:
         self.cap.release()
 
 # Manual fallback if no camera is detected
-def get_camera():
-    cam = Camera()
+def get_camera(device_index=0, width=640, height=480):
+    cam = Camera(device_index=device_index, width=width, height=height)
     if cam.cap.isOpened():
         return cam
     print("[WARN] No camera found — switching to manual input mode")
+    cam.release()
     return None 
