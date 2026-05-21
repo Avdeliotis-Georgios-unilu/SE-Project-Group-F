675559882d8a9324fc66588dd97782f83d5b82e4
Avdeliotis Georgios <georgios.avdeliotis@gmail.com>
2026-05-21 20:47:07 +0200

    final: ui.py

--- src/smart-rps/ui/ui.py
+++ src/smart-rps/ui/ui.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 _FILE_DIR = os.path.dirname(os.path.abspath(__file__))
-_PROJ_ROOT = os.path.dirname(os.path.dirname(_FILE_DIR))
+_PROJ_ROOT = os.path.dirname(_FILE_DIR)
 if _PROJ_ROOT not in sys.path:
     sys.path.insert(0, _PROJ_ROOT)
 
